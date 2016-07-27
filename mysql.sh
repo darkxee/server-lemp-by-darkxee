cdfcdb60c6c4f71313b984ec590aa6fe752d7a7b
darkxee <app.chittrakorn@gmail.com>
2016-07-27 14:04:35 +0700

    first

---
+++ mysql.sh
@@ -0,0 +1,3 @@
+sudo apt-get purge mysql-server* -y
+sudo apt-get install mysql-server-5.6 -y
+sudo service mysql restart
---
+++ nginx.sh
@@ -0,0 +1,5 @@
+sudo apt-get install \
+    nginx \
+    --yes
+
+sudo service nginx restart
