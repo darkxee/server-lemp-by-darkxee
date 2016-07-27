97fd40256b37e692f0cd968211a6c4ad2672bd16
darkxee <app.chittrakorn@gmail.com>
2016-07-27 15:10:47 +0700

    php update

--- 3php7.sh
+++ 3php7.sh
@@ -4,6 +4,11 @@ sudo apt-get update
 
 sudo apt-get remove php5-common -y
 sudo apt-get purge php5-common -y
-sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
+sudo apt-get install -y php7.0-fpm php7.0-mysql php7.0-cli php7.0-mcrypt php7.0-curl php7.0-gd php7.0-intl php7.0-xsl php7.0-zip php7.0-mbstring
+
 sudo apt-get --purge autoremove -y
 sudo service php7.0-fpm restart
+
+
+
+
